f5299dfdc0e79f466f8d2f0bd50ae0a2a3a06713
Michal Stabrawa <mistaaqua@gmail.com>
2017-10-19 21:58:08 +0200

    qqq

--- js/main.js
+++ js/main.js
@@ -83,3 +83,29 @@ function show1Skills(){
 }
 document.getElementById('show1').addEventListener('click', show1Skills);
 
+
+
+$(document).ready(function() {
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('h2').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},800);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
+
